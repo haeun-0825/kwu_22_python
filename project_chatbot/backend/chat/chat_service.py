ff3e737bd989021f87c2c5cf0c1cabb8a6e374f0
haeun-0825 <jeonhaeun0825@gmail.com>
2025-11-26 10:45:54 +0900

    Update fnc chatbot

--- project_chatbot/backend/chat/chat_service.py
+++ project_chatbot/backend/chat/chat_service.py
@@ -1,9 +1,17 @@
 import openai
+from chat.chat_llm import ChatLLM
+
 
 class ChatService:
     
     def __init__(self):
         pass
     
+    # 사용자로부터 질문(Query)를 받아 답변(Answer)을 생성하고 전달하는 기능
     async def gen_answer(self, query: str) -> str:
-        return "저도 반가워요~"
+        
+        # 답변 생성
+        chat_llm = ChatLLM()
+        answer = chat_llm.multiturn_chat(query)
+        
+        return answer
