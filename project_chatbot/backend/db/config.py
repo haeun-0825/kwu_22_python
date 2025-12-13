ba0ac5278ba907d3b5e570054058cf46bfce9f65
haeun-0825 <jeonhaeun0825@gmail.com>
2025-12-13 16:15:16 +0900

    Real Complete

--- project_chatbot/backend/db/config.py
+++ project_chatbot/backend/db/config.py
@@ -6,12 +6,12 @@ _ = load_dotenv(find_dotenv())
 
 class Settings:
     
-    DB_USERNAME : str = os.getenv("MARIADB_USER")
-    DB_PASSWORD : str = os.getenv("MARIADB_PASSWORD")
+    DB_USERNAME : str = os.getenv("MARIADB_USER","root")
+    DB_PASSWORD : str = os.getenv("MARIADB_PASSWORD", "mariadb")
     DB_HOST : str = os.getenv("MARIADB_HOST", "localhost")
     DB_PORT : str = os.getenv("MARIADB_PORT", 3306)
-    DB_DATABASE : str = os.getenv("MARIADB_DATABASE")
-    DB_URL = f"mariadb+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+    DB_DATABASE : str = os.getenv("MARIADB_DATABASE", "kwu")
+    DB_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
     
     
 @lru_cache()
